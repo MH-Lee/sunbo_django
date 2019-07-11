ca8bf5502871cb3dfafbf7c83b385dda6dda4344
MH-Lee <lmhoon012@gmail.com>
2019-07-12 05:35:13 +0900

    search function

--- information/urls.py
+++ information/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('rescue_list/', views.rescue_list),
-    path('dart_list/', views.dart_list),
-    path('rescue_detail/<int:pk>', views.rescue_detail),
+    path('rescue_list/', views.rescue_list, name='rescue_list'),
+    path('dart_list/', views.dart_list, name='dart_list'),
+    path('rescue_detail/<int:pk>', views.rescue_detail, name='rescue_detail'),
 ]
--- information/views.py
+++ information/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse
 from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import Rescue, Dart
+# from .forms import InfoSearchForm
 
 # Create your views here.
 def rescue_detail(request, pk):
@@ -14,18 +16,46 @@ def rescue_detail(request, pk):
 def rescue_list(request):
     if not request.user.is_authenticated:
         return redirect('/accounts/login/')
-    all_rescue = Rescue.objects.all().order_by('-id')
+    if request.method == 'GET':
+        query =  request.GET.get('q')
+        try:
+            rescue_obj = Rescue.objects.filter(
+                Q(area__icontains=query) | Q(case_num__icontains=query) |\
+                Q(subject__icontains=query) | Q(category__icontains=query) |\
+                Q(news_title__icontains=query)
+            ).order_by('-id')
+        except:
+            rescue_obj = Rescue.objects.all().order_by('-id')    
+    else:
+        rescue_obj = Rescue.objects.all().order_by('-id')
     page = int(request.GET.get('p',1))
-    paginator = Paginator(all_rescue, 20)
-    rescues = paginator.get_page(page)
+    try:
+        paginator = Paginator(rescue_obj, 15)
+        rescues = paginator.get_page(page)
+    except:
+        paginator = Paginator(rescue_obj, 15)
+        rescues = NULL
     return render(request, 'information/rescue_list.html', {'rescues':rescues})
 
 # dart_view
 def dart_list(request):
     if not request.user.is_authenticated:
         return redirect('/accounts/login/')
-    all_dart = Dart.objects.all().order_by('-id')
+    if request.method == 'GET':
+        query =  request.GET.get('q')
+        try:
+            darts = Dart.objects.filter(
+                Q(company_name__icontains=query) | Q(news_title__icontains=query) |  Q(another_name__icontains=query)
+            ).order_by('-id')
+        except:
+            darts = Dart.objects.all().order_by('-id')    
+    else:
+        darts = Dart.objects.all().order_by('-id')
     page = int(request.GET.get('p',1))
-    paginator = Paginator(all_dart, 15)
-    dart_infos = paginator.get_page(page)
+    try:
+        paginator = Paginator(darts, 15)
+        dart_infos = paginator.get_page(page)
+    except:
+        paginator = Paginator(darts, 15)
+        dart_infos = NULL
     return render(request, 'information/dart_list.html', {'dart_infos':dart_infos})
