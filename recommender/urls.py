307eef50708cf1d32992606a66d822dbb33f385b
MH-Lee <lmhoon012@gmail.com>
2019-10-30 20:13:46 +0900

    network 완성

--- recommender/urls.py
+++ recommender/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
+
 
 urlpatterns = [
     path('introduction/', views.introduction, name='service_intro'),
+    path('network/', views.network, name='network'),
+    path('startup/', TemplateView.as_view(template_name="./project/st_net2.html"), name='startup'),
 ]
--- recommender/views.py
+++ recommender/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def introduction(request):
     return render(request, './project/introduction.html')
+
+def network(request):
+    return render(request, './project/networks.html')
