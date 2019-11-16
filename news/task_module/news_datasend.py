5547869df15c74dfb11e98d112e8c9238fa7660b
MH-Lee <lmhoon012@gmail.com>
2019-11-17 01:22:26 +0900

    docker 삭제

--- news/task_module/news_datasend.py
+++ news/task_module/news_datasend.py
@@ -27,11 +27,6 @@ from gensim.summarization import keywords
 from newspaper import Article
 from newspaper import fulltext
 from news.task_module.news_crawler import NaverNewsCrawler
-from news.models import (LPCompany, 
-                         MainCompany, 
-                         InvestNews,
-                         Professor,
-                         Portfolio)
 from accounts.models import User
 
 class DataProcessingSend:
@@ -189,92 +184,9 @@ class DataProcessingSend:
         professor_result = pd.DataFrame(second_result,columns=['date','title','predicted_label1','predicted_label2','link','press'])
         return professor_result
 
-    def prof_send(self, data):
-        prof_list = []
-        for i in range(data.shape[0]):
-            media = data.loc[i, 'press']
-            news_title = data.iloc[i, 'title']
-            date = data.iloc[i,2].strftime('%Y-%m-%d')
-            small_class_1 = data.iloc[i,'predicted_label1']
-            small_class_2 = data.iloc[i,'predicted_label2']
-            news_url = data.iloc[i,'link']
-            writer = User.objects.get(username='admin')
-            prof_obj = Professor(media=media, date=date, small_class_1=small_class_1, small_class_2=small_class_2,\
-                                news_title=news_title, news_url=news_url, writer=writer)
-            prof_list.append(prof_obj)
-        Professor.objects.bulk_create(prof_list)
-        print('교수개발 업로드')
-
-    def invest_news_send(self, data):
-        invest_list = []
-        for i in range(data.shape[0]):
-            media = data.loc[i,0]
-            date =  data.loc[i,2].strftime('%Y-%m-%d')
-            news_title = data.loc[i,1]
-            news_url = data.loc[i,3]
-            writer = User.objects.get(username='admin')
-            invest_obj = InvestNews(media=media, date=date, news_title=news_title,\
-                                    news_url=news_url, writer=writer)
-            invest_list.append(invest_obj)
-        InvestNews.objects.bulk_create(invest_list)
-        print('invest_news 업로드')
-
-    def LP_company_send(self, data):
-        lpc_list = []
-        for i in range(data.shape[0]):
-            category = data.iloc[i,0]
-            company_name  = data.iloc[i,1]
-            news_title = data.iloc[i,2]
-            media = data.iloc[i,3]
-            date =  data.iloc[i,4].strftime('%Y-%m-%d')
-            print(date)
-            news_url = data.iloc[i,5]
-            writer = User.objects.get(username='admin')
-            lpc_obj = LPCompany(category=category, company_name=company_name, media=media, date=date,\
-                                    news_title=news_title, news_url=news_url, writer=writer)
-            lpc_list.append(lpc_obj)
-        LPCompany.objects.bulk_create(lpc_list)
-        print('LP기업 업로드')
-
-    def main_company_send(self, data):
-        main_list = []
-        for i in range(data.shape[0]):
-            category = data.iloc[i,0]
-            company_name  = data.iloc[i,1]
-            news_title = data.iloc[i,2]
-            media = data.iloc[i,3]
-            date =  data.iloc[i,4].strftime('%Y-%m-%d')
-            news_url = data.iloc[i,5]
-            writer = User.objects.get(username='admin')
-            main_obj = MainCompany(category=category, company_name=company_name, media=media, date=date,\
-                                    news_title=news_title, news_url=news_url, writer=writer)
-            main_list.append(main_obj)
-        MainCompany.objects.bulk_create(main_list)
-        print('Main기업 업로드')
-
-    def portfolio_send(self, data):
-        port_list = []
-        for i in range(data.shape[0]):
-            category = data.iloc[i,0]
-            company_name  = data.iloc[i,1]
-            news_title = data.iloc[i,2]
-            media = data.iloc[i,3]
-            date =  data.iloc[i,4].strftime('%Y-%m-%d')
-            news_url = data.iloc[i,5]
-            writer = User.objects.get(username='admin')
-            port_obj = Portfolio(category=category, company_name=company_name, media=media, date=date,\
-                                    news_title=news_title, news_url=news_url, writer=writer)
-            port_list.append(port_obj)
-        Portfolio.objects.bulk_create(port_list)
-        print('포트폴리오 업로드')
 
     def data_send(self):
         professor_last = self.professor_prediction()
-        invest = self.Naver.naver_crawler_exe(mode='invest')
-        company = self.Naver.naver_crawler_exe(mode='company')
-        port = company[company['category'] == '포트폴리오'].reset_index(drop=True)
-        lp = company[company['category'] == 'LP기업'].reset_index(drop=True)
-        main = company[company['category'] == '자매기업'].reset_index(drop=True)
         print("back up data save")
         professor_last.to_csv(self.path2 + "/backup_data/" + datetime.today().strftime("%Y%m%d")+ "_Professor_Development_naver.csv",  encoding = "utf-8-sig", header=True, index=False) 
         company.to_csv(self.path2 + "/backup_data/" + datetime.today().strftime("%Y%m%d")+ "_company_naver.csv",  encoding = "utf-8-sig", header=True, index=False) 
